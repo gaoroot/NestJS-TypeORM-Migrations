cd3ec6eb5247a81da13ceebc42f3dc1e1b78b229
Alexander Govorin <govorinao@gmail.com>
2024-01-27 01:36:00 +0000

    fix typeorm.ts config (npm run start:dev)

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -11,8 +11,10 @@ const config = {
   username: `${process.env.DATABASE_USERNAME}`,
   password: `${process.env.DATABASE_PASSWORD}`,
   database: `${process.env.DATABASE_NAME}`,
-  entities: ['*/**/*.entity.ts'],
-  migrations: ['src/migrations/*.ts'],
+  // entities: ['*/**/*.entity.{js,ts}'],
+  // migrations: ['src/migrations/*.{js,ts}'],
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/migrations/*.{js,ts}'],
   autoLoadEntities: true,
   synchronize: false,
 };
--- src/migrations/1706315984462-auto.ts
+++ src/migrations/1706315984462-auto.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class Auto1706265423907 implements MigrationInterface {
-    name = 'Auto1706265423907'
+export class Auto1706315984462 implements MigrationInterface {
+    name = 'Auto1706315984462'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "category" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, CONSTRAINT "PK_9c4e4a89e3674fc9f382d733f03" PRIMARY KEY ("id"))`);
